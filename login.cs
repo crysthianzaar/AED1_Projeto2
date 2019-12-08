5104ae8944a88b28801bcddb1961967bb6ca8881
thiagomtd2 <50428896+thiagomtd2@users.noreply.github.com>
2019-12-07 22:54:31 -0300

    Update login.cs

--- login.cs
+++ login.cs
@@ -1,18 +1,27 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 
 class login
 {
   public bool validaSenha(string senha){
     StreamReader arquivo;
-    arquivo = File.OpenText("senha.txt");
-    string original = arquivo.ReadLine();
-    if (senha==original){
-      return true;
+    arquivo = File.OpenText("Database/Usuarios/senha.txt");
+    string original = "";// arquivo.ReadLine();
+    string s;
+    var Usuarios = new List<usuario>();
+
+    while ((s = arquivo.ReadLine()) != null) 
+    {
+      var array = s.Split(';');
+      var usuario = new usuario();
+      usuario.Set_cpf(array[0]);
+      usuario.SetNome(array[1]);
+      usuario.SetNome(array[2]);
+      usuario.SetIdade(int.Parse(array[3]));
+      Usuarios.Add(usuario);
     }
-     else{
-     return false;
-    }
+    return Usuarios.Any(x => x.Get_cpf()==original);
   }
 }
